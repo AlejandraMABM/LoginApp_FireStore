bd789fd44f80d24297c70bdb88d06abe1f012ee7
Alejandra <baezmaria@hotmail.com>
2025-01-16 19:23:44 +0100

    added Firebase dependences, Implementations Firebase Aut

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func createUser(_ sender: Any) {
+        Auth.auth().createUser(withEmail: usernameTextField.text!,  password:passwordTextField.text!) { authResult, error in
+            if let error = error {
+                // hubi un error
+                print(error)
+                
+                let alertController = UIAlertController(title: "Create User", message: error.localizedDescription, preferredStyle: .alert)
 
+        
+
+                alertController.addAction(UIAlertAction(title: "OK", style: .default) )
+
+                self.present(alertController, animated: true, completion: nil)
+                
+            } else {
+                // todo correcto
+                print("User signs up successfully")
+                let alertController = UIAlertController(title: "Create User", message: "User created successfully", preferredStyle: .alert)
+
+        
+
+                alertController.addAction(UIAlertAction(title: "OK", style: .default) )
+
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: usernameTextField.text!,  password:passwordTextField.text!) { [unowned self] authResult, error in
+            // guard let strongSelf = self else { return }
+            if let error = error {
+                let alertController = UIAlertController(title: "Sign In", message: error.localizedDescription, preferredStyle: .alert)
+                
+                
+                
+                alertController.addAction(UIAlertAction(title: "OK", style: .default) )
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+            } else {
+                print("User signs up successfully")
+                self.performSegue(withIdentifier: "goToHome", sender:nil)
+                
+                
+            }
+        }
+    }
 }
 
